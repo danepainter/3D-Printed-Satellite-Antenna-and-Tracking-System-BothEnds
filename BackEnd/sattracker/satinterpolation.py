4561001b6b59c2b0871641e105f08c6a5826b2fe
rsalt <rsalt@DESKTOP-JUBA504>
2025-10-18 20:00:55 -0400

    Documentation for sattracker module

--- BackEnd/sattracker/satinterpolation.py
+++ BackEnd/sattracker/satinterpolation.py
@@ -1,14 +1,41 @@
 import numpy as np
 
 
-def convert_to_python_values(times_num, azs_num, els_num):
+def _convert_to_python_values(times_num: np.ndarray, azs_num: np.ndarray, els_num: np.ndarray):
+    """Converts numpy values to pythonic values
+
+    Args:
+        times_num:
+            Numpy array of time data points
+        azs_num:
+            Numpy array of azimuth data points
+        els_num:
+            Numpy array of elevation data points
+
+    Returns:
+        A list of tuples containing the pythonic versions of the data points
+    """
     python_times = [time.item() for time in times_num]
     python_azs = [az.item() for az in azs_num]
     python_els = [el.item() for el in els_num]
     return list(zip(python_azs, python_els, python_times))
 
 
-def round_data(azs_num, els_num, rounding_override=0):
+def _round_data(azs_num: np.ndarray, els_num: np.ndarray, rounding_override: int = 0):
+    """Rounds azimuth and elevation data points
+
+    Args:
+        azs_num:
+            Numpy array of azimuth data points
+        els_num:
+            Numpy array of elevation data points
+        rounding_override:
+            An int representing to what decimal place should the points be rounded.
+            0 is whole number rounding and >0 equates to decimal places
+
+    Returns:
+        Two numpy arrays of rounded azimuth and elevation data points
+    """
     if rounding_override != 0:
         azs_rounded = np.round(azs_num, rounding_override)
         els_rounded = np.round(els_num, rounding_override)
@@ -17,24 +44,47 @@ def round_data(azs_num, els_num, rounding_override=0):
 
 
 class SatInterpolation:
+    """Handles the path interpolation of a satellite
+
+    Attributes:
+        pass_data:
+            A sat_pass object containing satellite pass data
+    """
     def __init__(self, pass_data):
+        """Initializes instance with satellite pass data object
+
+        Args:
+            pass_data:
+                A SatPass object containing satellite pass data
+        """
         self.pass_data = pass_data
 
-    def interp_satellite_path(self, interpolation_second_steps=1, rounding_override=0):
-        # get times
-        t0, tm, t1 = self.pass_data.startUTC, self.pass_data.maxUTC, self.pass_data.endUTC
+    def interp_satellite_path(self, interpolation_second_steps: int = 1, rounding_override: int = 0):
+        """Interpolates the satellite path
+
+        Args:
+            interpolation_second_steps:
+                An int representing how often to interpolate the path
+            rounding_override:
+                An int representing to what decimal place should the points be
+                rounded. 0 is whole number rounding and >0 equates to decimal places
+
+        Returns:
+            A list of interpolated data points that have been rounded and
+            converted to pythonic values
+        """
+        t0, tm, t1 = self.pass_data.startUTC, self.pass_data.maxUTC, self.pass_data.endUTC  # get times
         key_t = np.array([t0, tm, t1], dtype=float)
 
-        # convert az from degrees to radians
+        # converts az from degrees to radians
         az_deg = np.array([self.pass_data.startAz, self.pass_data.maxAz, self.pass_data.endAz], dtype=float)
         az_rad = np.radians(az_deg)
         az_rad_unwrapped = np.unwrap(az_rad)  # handles data bigger than 360 deg/2pi rad
 
-        # get elevations
-        el = np.array([self.pass_data.startEl, self.pass_data.maxEl, self.pass_data.endEl], dtype=float)
+        el = np.array([self.pass_data.startEl, self.pass_data.maxEl,
+                       self.pass_data.endEl], dtype=float)  # get elevations
 
-        # sample times (UTC seconds)
-        times = np.arange(t0, t1 + 1, interpolation_second_steps).astype(float)
+        times = np.arange(t0, t1 + 1, interpolation_second_steps).astype(float)  # sample times (UTC seconds)
 
         # interpolate in radians for az, degrees for el
         az_interp_rad = np.interp(times, key_t, az_rad_unwrapped)
@@ -43,6 +93,6 @@ class SatInterpolation:
         # convert az back to degrees for serial
         az_interp_deg = (np.degrees(az_interp_rad) + 360.0) % 360.0
 
-        az_interp_deg, el_interp_deg = round_data(az_interp_deg, el_interp_deg, rounding_override)
-        return convert_to_python_values(times, az_interp_deg, el_interp_deg)
+        az_interp_deg, el_interp_deg = _round_data(az_interp_deg, el_interp_deg, rounding_override)
+        return _convert_to_python_values(times, az_interp_deg, el_interp_deg)
 
